f7b8b4833622c605f5003c333fbac7e02cbbff33
Nic4Las <griethe.jcf@gmail.com>
2024-11-19 19:19:30 +0100

    fixed typo

--- src/lib/NovelChunkedExporter.svelte
+++ src/lib/NovelChunkedExporter.svelte
@@ -34,7 +34,7 @@
     const parser = new DOMParser();
     const limit = pRateLimit({
         interval: 10000, // 1000 ms == 1 second
-        rate: , // 30 API calls per interval
+        rate: 5, // 30 API calls per interval
         concurrency: 4, // no more than 10 running at once
     });
 
